e754e5a8283e1b4e69ce8f583a2221d2d638dcf6
DMG-01 <110745735+DMG-01@users.noreply.github.com>
2024-08-30 23:57:26 +0100

    .

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,31 @@
-const express 
+const express = require("express")
+const app = express()
+let  {people} = require("./data")
+
+app.use(express.static("./methods-public"))
+app.use(express.urlencoded({extended:false}))
+app.use(express.json)
+
+app.get("/api/people",(req,res)=> {
+    res.status(200).json({success:true,data:people})
+})
+
+
+app.post("/api/people",(req,res)=> {
+    const {name} = req.body
+    if(!name) {
+        return res.status(400).json({success:false,msg:"please provide name value"})
+    }
+    res.status(201).json({}) 
+})
+
+app.post("/login",(req,res)=> {
+    const {name} = req.body
+    if(name === "") {
+        return res.status(401).send("enter a name")
+    }
+    res.status(200).send(`Welcnaome ${name}`)
+})
+app.listen(3000,()=> {
+    console.log("server listening on port 3000...") 
+})
