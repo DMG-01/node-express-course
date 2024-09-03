3f0fda508b539693094e0d148e9db720c3e0aba4
DMG-01 <110745735+DMG-01@users.noreply.github.com>
2024-09-03 22:48:13 +0100

    .

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -16,8 +16,9 @@ app.use("/api/v1/tasks",task)
 const port = 3000
 const start = async()=> {
     try {
-        await connectDb(process.env.MONGO_URI)
+       // await connectDb("mongodb+srv://femi:1234@nodeexpresstut.z5tzs.mongodb.net/?retryWrites=true&w=majority&appName=nodeExpressTut"/*process.env.MONGO_URI*/)
         console.log(`server is listening in port ${port}`) 
+        console.log("connected to database")
     }catch(error) {
         console.log(error)
     }
