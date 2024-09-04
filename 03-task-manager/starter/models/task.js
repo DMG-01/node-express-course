ba5a368ad822790429769adf57c4fa30bc73dfb7
DMG-01 <110745735+DMG-01@users.noreply.github.com>
2024-09-04 23:36:34 +0100

    node

--- 03-task-manager/starter/models/task.js
+++ 03-task-manager/starter/models/task.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose")
 
 const taskSchema = new mongoose.Schema({
-    name: String,
-    completed : Boolean
+    name: {
+    type:String,
+    required:[true,"must provide name"],
+    trim:true,
+    maxlength:[20,"max character is 20"]
+    },
+    completed : {
+        type:Boolean,
+        default:false
+    }
 })
 
 module.exports = mongoose.model("Task",taskSchema)
